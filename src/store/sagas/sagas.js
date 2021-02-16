ff6196490d5fb8fb26b3a57458161fd6f478ecdf
Kach <kach_work@ukr.net>
2021-02-16 13:30:35 +0200

    fixes for lesson-3

--- src/store/sagas/sagas.js
+++ src/store/sagas/sagas.js
@@ -1,9 +1,5 @@
 import { all } from 'redux-saga/effects';
 
-function* helloSaga() {
-  yield console.log('Hello world!');
-}
-
 export default function* rootSaga() {
-  yield all(helloSaga());
+  yield all();
 }
